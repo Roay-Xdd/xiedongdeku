896aa3f8974d01ff4d14b2cb98e5a6e44d5cb66b
shanshan-857 <429549867@qq.com>
2020-07-03 21:06:49 +0800

    鹏哥提交

---
+++ common_service/src/main/java/com/wsy/entity/config.java
@@ -0,0 +1,7 @@
+package com.wsy.entity;
+
+public class config {
+    public static void main(String[] args) {
+        System.out.println(1111);
+    }
+}
---
+++ common_service/src/main/java/com/wsy/entity/ss.java
@@ -0,0 +1,5 @@
+package com.wsy.entity;
+
+public class ss {
+
+}
